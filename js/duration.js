9c73a389b8dcb278aea539299ee8fa67fe1568ef
rickyxume <rickyxu.me@gmail.com>
2021-03-14 14:44:49 +0800

    Site updated: 2021-03-14 14:44:49

--- js/duration.js
+++ js/duration.js
@@ -20,7 +20,7 @@
       if(String(snum).length === 1 ){
         snum = "0" + snum;
       }
-      document.getElementById("timeDate").innerHTML = "划水&nbsp"+dnum+"&nbsp天";
+      document.getElementById("timeDate").innerHTML = "已经在池塘里划水了&nbsp"+dnum+"&nbsp天";
       document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
     }
     setInterval(update, 1000);
